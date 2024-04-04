034595763bd2a88a54347a354fd05d5dfdb59ddf
Pouria Khalaj <pouria.khalaj@smhi.se>
2024-04-04 15:59:36 +0200

    Remove all traces of SQL including files and references

--- setup.py
+++ setup.py
@@ -28,10 +28,6 @@ import imp
 version = imp.load_source('trolldb.version', 'trolldb/version.py')
 
 
-# requirements = ['geoalchemy2', 'sqlalchemy>=1.3.0', 'pyorbital',
-#                 'posttroll', 'shapely', 'psycopg2', 'paramiko',
-#                 'pymongo']
-
 requirements = ['pymongo', 'pyyaml', 'posttroll']
 
 setup(name="pytroll-db",
